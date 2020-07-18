af5488c7e14e129d90b13b6cf8d929a973082b3b
tatianalopes <tcalopes19@gmail.com>
2020-07-18 19:27:40 -0300

    Add .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
+import config from './utils/config';
 import routes from './routes';
 
 const app = express();
@@ -9,4 +10,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(config.PORT);
